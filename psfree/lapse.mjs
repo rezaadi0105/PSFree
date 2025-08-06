a78b7fb4f33c1ebeda4065c7fdba97aec9b6bf61
rezaadi0105 <rezaadipangestu5@gmail.com>
2025-08-06 23:58:12 +0700

    Add message after payloads success
    Signed-off-by: rezaadi0105 <rezaadipangestu5@gmail.com>

--- psfree/lapse.mjs
+++ psfree/lapse.mjs
@@ -174,6 +174,10 @@ function call_nze(...args) {
     }
 }
 
+function allset(){
+    msgs.innerHTML="PS4 Exploited And Payloads Loaded.";
+}
+
 // #define SCE_KERNEL_AIO_STATE_NOTIFIED       0x10000
 //
 // #define SCE_KERNEL_AIO_STATE_SUBMITTED      1
@@ -1849,7 +1853,8 @@ var loader_addr = chain.sysp(
         0,
         loader_addr,
         payload_buffer,
-    );	
+    );
+    allset();
    }
  };
 
